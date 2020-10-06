fcba36de7d9c4d98255a9828fc4394fc6a2e9a99
kazuhitonakayama <firstjin19990101@gmail.com>
2020-10-06 19:58:11 +0900

    d

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,8 @@
 class MoviesController < ApplicationController
+  require 'themoviedb-api'
+  Tmdb::Api.key("64721be267690b71440c6694a651abc1")
+
+  
   def search
     @search_term = params[:looking_for]
     @movie_results = Movie.search(@search_term)
