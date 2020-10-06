2803801f2abd1f4bb84483ef8cda04138a9fc9b0
kazuhitonakayama <firstjin19990101@gmail.com>
2020-10-07 01:03:54 +0900

    検索したら映画情報が出るようになった

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,11 @@
 class MoviesController < ApplicationController
   require 'themoviedb-api'
   Tmdb::Api.key("64721be267690b71440c6694a651abc1")
+  Tmdb::Api.language("ja")
+
 
 
   def search
-    @movies = Tmdb::Search.movie("Harry")
   end
   
   def show
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,21 +1,4 @@
 class Movie < ApplicationRecord
-    include HTTParty
 
-    # default_options.update(verify: false) # disable SSL verification(必要に応じて)
-    default_params api_key: '64721be267690b71440c6694a651abc1', language: 'ja-JP' #共通パラメタ                 
-    format :json
-
-    # キーワードによる検索機能
-    # https://developers.themoviedb.org/3/search/search-keywordsに参照
-    def self.search term
-    base_uri 'https://api.themoviedb.org/3/search/movie'
-    get("", query: { query: term }) # {}の中身はパラメタ
-    end
-
-    # 指定の映画の詳細情報を取得
-    # https://developers.themoviedb.org/3/movies/get-movie-detailsに参照
-    def self.details id
-    base_uri "https://api.themoviedb.org/3/movie/#{id}"
-    get("", query: { } ) #パラメタなし
-    end
+    
 end
