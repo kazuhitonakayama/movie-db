13ac246985801f1eba5c6c87c7e2a56109a987cd
kazuhitonakayama <firstjin19990101@gmail.com>
2020-10-06 18:53:45 +0900

    API set

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
   def search
+    @search_term = params[:looking_for]
+    @movie_results = Movie.search(@search_term)
   end
-
+  
   def show
+    @movie_info = Movie.details(params[:id])
   end
 end
