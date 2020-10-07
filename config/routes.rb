862e5e448e3545b5da22cb74fb6cbd2ca4fb1358
kazuhitonakayama <firstjin19990101@gmail.com>
2020-10-07 13:26:44 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'movies/search'
+  root 'movies/search'
   get 'movies/show'
-  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
